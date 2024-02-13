70ec2529e419511930ae38e7931c2644f71fbae5
unknown <maxsuel.dev@gmail.com>
2024-02-13 08:24:19 -0300

    mobile first

--- js/script.js
+++ js/script.js
@@ -3,4 +3,7 @@ const listaHamburger = document.querySelector('#lista-menu');
 
 menuHamburger.addEventListener('click', () => {
     listaHamburger.classList.toggle('active');
+    const isActive = listaHamburger.classList.contains('active');
+
+    menuHamburger.classList = isActive ? 'fas fa-times fa-lg hamburger' : 'fas fa-bars fa-lg hamburger';
 });
